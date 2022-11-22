d9a47ca0e470e4b9c4beb3f559d9b9b470ba3e7c
dinbecarevic <din.velenje@gmail.com>
2022-11-22 08:55:12 +0100

    Notice in alert v navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_notice
+      content_tag(:p, notice, class: 'alert alert-success') if notice.present?
+  end
+
+  def display_alert
+      content_tag(:p, alert, class: 'alert alert-danger') if alert.present?
+  end
 end
