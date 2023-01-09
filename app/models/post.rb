ba93d74310e8b78964c2364b31eaff76018dab45
dinbecarevic <din.velenje@gmail.com>
2023-01-09 21:23:00 +0100

    add rich text in fontawesome

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   scope :published, -> { where(published_at: nil).or(where("published_at <= ?", DateTime.now)) }
   # en bug je da je timezona UTC +00:00 oz. GMT oz. Greenwich Mean Time in mormo objave dejansko 1 uro prej objaviti aš ka mislim
+  has_rich_text :body
 end
